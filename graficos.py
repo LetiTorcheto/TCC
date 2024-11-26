33705c956b52fe339757803391aedcb00759121e
Leticia Torcheto Ramos <ramos.letitorcheto@gmail.com>
2024-11-26 00:55:54 -0300

    adicionar tempo em horas

--- graficos.py
+++ graficos.py
@@ -4,24 +4,40 @@ import requests
 from streamlit_echarts import st_pyecharts
 from pyecharts.charts import Line
 from pyecharts import options as opts
+import pytz 
+
 
 
 @st.cache_data
 def data_coletas():
-    requisicao = requests.get("https://metlab.rexlab.ufsc.br/api/dados")
+    requisicao = requests.get("https://metlab.rexlab.ufsc.br/api/dados?qntPorPag=10000")
     dados = requisicao.json()['coletas']
     df = pd.DataFrame(dados)
     df['dataDoEnvio'] = pd.to_datetime(df['dataDoEnvio'])
     return df
+
 df = data_coletas()
 
-st.sidebar.header("Filtragem de Dados por Data")
+st.sidebar.header("Filtragem de Dados por Data e Horário")
+
+# Filtro por data
 data_inicio = st.sidebar.date_input("Data de Início", df['dataDoEnvio'].min().date())
 data_fim = st.sidebar.date_input("Data de Fim", df['dataDoEnvio'].max().date())
 
+# Filtro por horário
+timezone = pytz.UTC
+horario_inicio = st.sidebar.time_input("Horário de início filtrado ", value=pd.Timestamp.min.time())
+horario_fim = st.sidebar.time_input("Horário de fim filtrado", value=pd.Timestamp.max.time())
 
-df_filtrado = df[(df['dataDoEnvio'].dt.date >= data_inicio) & (df['dataDoEnvio'].dt.date <= data_fim)]
+# Combina data e horário 
+datetime_inicio = timezone.localize(pd.Timestamp(f"{data_inicio} {horario_inicio}"))
+datetime_fim = timezone.localize(pd.Timestamp(f"{data_fim} {horario_fim}"))
 
+
+# Filtra o DataFrame
+df_filtrado = df[(df['dataDoEnvio'] >= datetime_inicio) & (df['dataDoEnvio'] <= datetime_fim)]
+
+# Seleção do sensor
 sensores = ["temperatura", "umidadeAr", "umidadeSolo", "pressao", "iluminacao", 
             "nivelUV", "alcool", "tolueno", "acetona", "NH4", "CO", "CO2"]
 
@@ -33,7 +49,7 @@ def plot_sensor_data(sensor):
         .add_xaxis(df_filtrado['dataDoEnvio'].dt.strftime("%Y-%m-%d %H:%M:%S").tolist())
         .add_yaxis(sensor, df_filtrado[sensor].tolist(), is_smooth=True)
         .set_global_opts(
-            title_opts=opts.TitleOpts(title=f"Leitura do Sensor: {sensor.capitalize()} - {data_inicio} até {data_fim}"),
+            title_opts=opts.TitleOpts(title=f"Leitura do Sensor: {sensor.capitalize()} - {datetime_inicio} até {datetime_fim}"),
             xaxis_opts=opts.AxisOpts(name="Data e Hora da Coleta", type_="category"),
             yaxis_opts=opts.AxisOpts(name=sensor.capitalize()),
             tooltip_opts=opts.TooltipOpts(trigger="axis"),
@@ -46,4 +62,4 @@ def plot_sensor_data(sensor):
 if not df_filtrado.empty:
     st_pyecharts(plot_sensor_data(sensor_selecionado))
 else:
-    st.write("Nenhuma coleta encontrada para o intervalo de datas selecionado.")
+    st.write("Nenhuma coleta encontrada para o intervalo de datas e horários selecionado.")
---
+++ readme.md
@@ -0,0 +1,1 @@
+Para iniciciar os testes dos graficos deve se iniciar a venv e acionar o comando streamlit run graficos.py  para criar um localhost para testes 
